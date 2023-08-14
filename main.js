61ac4160a424298087ab5f8a292b5990b38bd5ba
Adriana Stecalovici <adriana.stecalovici200@gmail.com>
2023-08-14 11:18:07 +0300

    Orders Page

--- main.js
+++ main.js
@@ -15,10 +15,23 @@ function getHomePageTemplate() {
   `;
 }
 
-function getOrdersPageTemplate() {
+/*function getOrdersPageTemplate() {
   return `
     <div id="content">
       <h1 class="text-2xl mb-4 mt-8 text-center">Purchased Tickets</h1>
+    </div>
+  `;
+}*/
+
+function getOrdersPageTemplate() {
+  return `
+    <div id="content" class="white-background">
+      <h1 class="text-2xl mb-4 mt-8 text-center">Purchased Tickets</h1>
+      <div class="sort-buttons">
+        <button class="btn btn-sort" id="sortAscendingBtn">Sort Ascending By Price</button>
+        <button class="btn btn-sort" id="sortDescendingBtn">Sort Descending By Price</button>
+      </div>
+      <div class="orders"></div>
     </div>
   `;
 }
@@ -45,98 +58,73 @@ function setupMobileMenuEvent() {
   }
 }
 
+/*function setupPopstateEvent() {
+  window.addEventListener('popstate', () => {
+    const currentUrl = window.location.pathname;
+    renderContent(currentUrl);
+  });
+}*/
+
 function setupPopstateEvent() {
   window.addEventListener('popstate', () => {
     const currentUrl = window.location.pathname;
     renderContent(currentUrl);
+    setupSortButtons(); // Adăugați această linie pentru a re-atașa evenimentele de sortare
+  });
+}
+
+/*function setupInitialPage() {
+  const initialUrl = window.location.pathname;
+  renderContent(initialUrl);
+}*/
+
+function setupSortButtons() {
+  const sortAscendingBtn = document.getElementById('sortAscendingBtn');
+  sortAscendingBtn.addEventListener('click', () => {
+    sortOrders(true);
+  });
+
+  const sortDescendingBtn = document.getElementById('sortDescendingBtn');
+  sortDescendingBtn.addEventListener('click', () => {
+    sortOrders(false);
   });
 }
 
 function setupInitialPage() {
   const initialUrl = window.location.pathname;
   renderContent(initialUrl);
+
+  setupSortButtons(); // Adăugați această linie pentru a atașa evenimentele de sortare inițiale
 }
 
-async function fetchTicketEvents() {
-  const response = await fetch('https://localhost:7245/api/Event/GetAll');
-  const data = await response.json();
-  return data;
-}
+async function renderOrders() {
+  const ordersData = await fetchOrders();
+  const ordersContainer = document.querySelector('.orders');
+  
+  // Golește containerul de comenzilor existente
+  ordersContainer.innerHTML = '';
 
-
-
-async function placeOrder(orderData) {
-
-  const url = 'http://localhost:8080/createOrder'; // Replace with your actual API endpoint
-
-  const options = {
-
-    method: 'POST',
-
-    headers: {
-
-      'Content-Type': 'application/json', // Set the appropriate content type
-
-      // Add any additional headers if needed
-
-    },
-
-    body: JSON.stringify(orderData), // Convert your data to JSON
-
-  };
-
- 
-
-  try {
-
-    const response = await fetch(url, options);
-
-    if (!response.ok) {
-
-      const errorMessage = await response.text(); // Get the error message from the response body
-
-      throw new Error(`Network response was not ok: ${response.status} - ${errorMessage}`);
-
-    }
-
-    const data = await response.json();
-
-    return data;
-
-  } catch (error) {
-
-    console.error('Fetch error:', error);
-
+  // Iterează prin comenzile sortate și adaugă-le la container
+  for (const orderData of ordersData) {
+    const orderCard = await renderOrderCard(orderData);
+    ordersContainer.appendChild(orderCard);
   }
-
 }
 
- 
+async function sortOrders(ascending) {
+  const ordersData = await fetchOrders();
+  ordersData.sort((a, b) => {
+    return ascending ? a.totalPrice - b.totalPrice : b.totalPrice - a.totalPrice;
+  });
 
-// Example order data
+  const ordersContainer = document.querySelector('.orders');
+  ordersContainer.innerHTML = ''; // Golește containerul de comenzilor
 
-const orderData = {
-
-  "ticketCategoryId": 1,
-
-  "eventId": 1,
-
-  "numberOfTickets": 2
-
-};
-
- 
-
-// Call the placeOrder function with the order data
-
-placeOrder(orderData).then(data => {
-
-  console.log('Order placed:', data);
-
-  // Process the response data as needed
-
-});
-
+  for (const orderData of ordersData) {
+    const orderCard = await renderOrderCard(orderData);
+    ordersContainer.appendChild(orderCard);
+  }
+}
 
 async function renderHomePage(eventsData) {
   const mainContentDiv = document.querySelector('.main-content-component');
@@ -226,6 +214,59 @@ console.log(orderData);
 }
 
 
+async function fetchTicketEvents() {
+  const response = await fetch('https://localhost:7245/api/Event/GetAll');
+  const data = await response.json();
+  return data;
+}
+
+async function fetchOrders() {
+  const response = await fetch('https://localhost:7245/api/Order/GetAll');
+  const orders = await response.json();
+
+  orders.forEach(order => {
+    order.totalPrice = parseFloat(order.totalPrice);
+  });
+
+  return orders;
+
+}
+
+async function placeOrder(orderData) {
+  const url = 'http://localhost:8080/createOrder'; // Replace with your actual API endpoint
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json', 
+    },
+    body: JSON.stringify(orderData), 
+  };
+
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      throw new Error(`Network response was not ok: ${response.status} - ${errorMessage}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
+}
+
+ 
+
+const orderData = {
+  "ticketCategoryId": 1,
+  "eventId": 1,
+  "numberOfTickets": 2
+};
+
+placeOrder(orderData).then(data => {
+  console.log('Order placed:', data);
+});
+
 function setupQuantityButtons() {
   const decreaseBtns = document.querySelectorAll('.quantity-btn.decrease');
   const increaseBtns = document.querySelectorAll('.quantity-btn.increase');
@@ -245,9 +286,49 @@ function setupQuantityButtons() {
     });
   });
 }
-function renderOrdersPage(categories) { //fara param
+
+async function renderOrderCard(orderData) {
+  const orderCard = document.createElement('div');
+  orderCard.classList.add('order-card');
+
+  const contentMarkup = `
+    <div class="order-details">
+      <p><strong>Order ID:</strong> ${orderData.orderId}</p>
+      <p><strong>Date:</strong> ${orderData.orderedAt}</p>
+      <p><strong>Ticket Category:</strong> ${orderData.ticketCategory}</p>
+      <p><strong>Number of Tickets:</strong> ${orderData.numberOfTickets}</p>
+      <p><strong>Total Price:</strong> ${orderData.totalPrice}</p>
+    </div>
+    <div class="order-actions">
+      <button class="btn btn-modify">Modify</button>
+      <button class="btn btn-delete">Delete</button>
+    </div>
+  `;
+
+  orderCard.innerHTML = contentMarkup;
+  return orderCard;
+}
+
+async function renderOrdersPage() {
   const mainContentDiv = document.querySelector('.main-content-component');
   mainContentDiv.innerHTML = getOrdersPageTemplate();
+
+  const ordersData = await fetchOrders();
+
+  const ordersContainer = document.createElement('div');
+  ordersContainer.classList.add('orders');
+
+  const ordersTitle = document.createElement('h1');
+  ordersTitle.classList.add('orders-title');
+  ordersContainer.appendChild(ordersTitle);
+
+  for (const orderData of ordersData) {
+    const orderCard = await renderOrderCard(orderData);
+    ordersContainer.appendChild(orderCard);
+  }
+
+  mainContentDiv.appendChild(ordersContainer);
+  setupSortButtons();
 }
 
 // Render content based on URL
